dae7dd8487650ca1bd28943418b62bae4988c385
Kyrylo Dorozhynskyi <kyrylo.dorozhynskyi@justdev.org>
2024-02-04 18:15:06 +0100

    [DEV]

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -5,7 +5,7 @@
  * Created Date: Sunday, February 4th 2024, 17:24:04
  * Author: Kirill Dorozhynskyi - kirilldy@justdev.org
  * -----
- * Last Modified: Sunday, February 4th 2024 18:11:49
+ * Last Modified: Sunday, February 4th 2024 18:15:02
  * Modified By: Kirill Dorozhynskyi
  * -----
  * Copyright (c) 2024 justDev
@@ -13,45 +13,14 @@
 
 import express, { Router } from "express";
 import serverless from "serverless-http";
-
-const puppeteer = require("puppeteer");
 const path = require("path");
-const api = express();
+const app = express();
+
+app.use(express.static(path.join(__dirname, "..", "dist")));
 
 const router = Router();
-router.get("/hello", async (req, res) => {
-  var url = "https://florian.justdev.link";
+router.get("/hello", (req, res) => res.send("Hello World!"));
 
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(url, {
-    waitUntil: "networkidle2",
-  });
-  await page.setViewport({ width: 1239, height: 1753 });
+app.use("/api/", router);
 
-  //   const pdf = await page.pdf({
-  //     path: "output.pdf",
-  //     format: "A4",
-  //     printBackground: true,
-  //     displayHeaderFooter: false,
-
-  //     margin: {
-  //       top: "0",
-  //       right: "0",
-  //       bottom: "0",
-  //       left: "0",
-  //     },
-  //   });
-
-  //   await browser.close();
-  //   res.set({
-  //     "Content-Type": "application/pdf",
-  //     "Content-Length": pdf.length,
-  //   });
-  //   const filePath = path.join(__dirname, "output.pdf");
-  //   res.sendFile(filePath);
-});
-
-api.use("/api/", router);
-
-export const handler = serverless(api);
+export const handler = serverless(app);
