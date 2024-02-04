95055f97d8ca937ded55fd13451af9725ec050b2
Kyrylo Dorozhynskyi <kyrylo.dorozhynskyi@justdev.org>
2024-02-04 20:45:36 +0100

    [DEV]

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -5,7 +5,7 @@
  * Created Date: Sunday, February 4th 2024, 19:05:54
  * Author: Kirill Dorozhynskyi - kirilldy@justdev.org
  * -----
- * Last Modified: Sunday, February 4th 2024 20:44:38
+ * Last Modified: Sunday, February 4th 2024 20:45:16
  * Modified By: Kirill Dorozhynskyi
  * -----
  * Copyright (c) 2024 justDev
@@ -15,13 +15,20 @@ import chromium from "@sparticuz/chromium";
 import puppeteer from "puppeteer-core";
 import express from "express";
 import serverless from "serverless-http";
-import path from "path";
 const FTPClient = require("ftp");
 
 const app = express();
 
 chromium.setHeadlessMode = true;
 chromium.setGraphicsMode = false;
+
+let ftp_client = new FTPClient();
+let ftpConfig = {
+  host: "localhost",
+  port: 21,
+  user: "test",
+  password: "test",
+};
 
 app.get("/api/hello", async (req, res) => {
   var url = "https://crm.justdev.link";
@@ -41,7 +48,30 @@ app.get("/api/hello", async (req, res) => {
   });
   await page.setViewport({ width: 1239, height: 1753 });
 
+  // const pdf = await page.pdf({
+  //   format: "A4",
+  //   printBackground: true,
+  //   displayHeaderFooter: false,
+  //   margin: {
+  //     top: "0",
+  //     right: "0",
+  //     bottom: "0",
+  //     left: "0",
+  //   },
+  // });
+
+  // await browser.close();
+
+  // // res.set({
+  // //   "Content-Type": "application/pdf",
+  // //   "Content-Length": pdf.length,
+  // // });
+
+  // res.contentType("application/pdf");
+  // res.send(pdf);
+
   const pdf = await page.pdf({
+    // path: "/tmp/output.pdf", // Save to /tmp directory
     format: "A4",
     printBackground: true,
     displayHeaderFooter: false,
@@ -60,31 +90,8 @@ app.get("/api/hello", async (req, res) => {
     "Content-Length": pdf.length,
   });
 
-  console.log(pdf);
-
-  // // // Uncomment the lines below if you want to send the PDF in the response
-  res.contentType("application/pdf");
-  // // res.send(pdf);
-
-  // // Uncomment the lines below if you want to upload the PDF via FTP
-  // let ftp_client = new FTPClient();
-  // let ftpConfig = {
-  //   host: "37.9.175.181",
-  //   port: 21,
-  //   user: "netify.justdev.link",
-  //   password: "Fi93~$7u=f",
-  // };
-
-  // ftp_client.connect(ftpConfig);
-
-  // ftp_client.on("ready", function () {
-  //   ftp_client.put(pdf, "output.pdf", function (err) {
-  //     if (err) throw err;
-  //     ftp_client.end();
-  //   });
-  // });
-
-  // Remove the redundant ftp_client.connect();
+  // const filePath = path.join(__dirname, "tmp/output.pdf"); // Update the file path
+  res.sendFile(pdf);
 });
 
 export const handler = serverless(app);
