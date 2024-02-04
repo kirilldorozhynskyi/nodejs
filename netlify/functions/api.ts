e992f91ec7aa804ef3279435bc1b9e84cd902cfc
Kyrylo Dorozhynskyi <kyrylo.dorozhynskyi@justdev.org>
2024-02-04 18:32:22 +0100

    [DEV]

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -5,7 +5,7 @@
  * Created Date: Sunday, February 4th 2024, 17:24:04
  * Author: Kirill Dorozhynskyi - kirilldy@justdev.org
  * -----
- * Last Modified: Sunday, February 4th 2024 18:31:02
+ * Last Modified: Sunday, February 4th 2024 18:32:19
  * Modified By: Kirill Dorozhynskyi
  * -----
  * Copyright (c) 2024 justDev
@@ -17,23 +17,21 @@ const path = require("path");
 const app = express();
 const puppeteer = require("puppeteer");
 
-(async () => {
+app.use(express.static(path.join(__dirname, "..", "dist")));
+
+app.get("/api/hello", async (req, res) => {
+  var url = "https://florian.justdev.link";
+
   const browser = await puppeteer.launch({
     executablePath: process.env.CHROME_BIN || "/usr/bin/chromium-browser",
   });
-  app.use(express.static(path.join(__dirname, "..", "dist")));
-
-  app.get("/api/hello", async (req, res) => {
-    var url = "https://florian.justdev.link";
-
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url, {
-      waitUntil: "networkidle2",
-    });
-    await page.setViewport({ width: 1239, height: 1753 });
-
-    res.send("Hello World!");
+  const page = await browser.newPage();
+  await page.goto(url, {
+    waitUntil: "networkidle2",
   });
-})();
+  await page.setViewport({ width: 1239, height: 1753 });
+
+  res.send("Hello World!");
+});
+
 export const handler = serverless(app);
