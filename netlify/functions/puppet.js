6770c26959bf0360d459044339da494030c1ac71
Kyrylo Dorozhynskyi <kyrylo.dorozhynskyi@justdev.org>
2024-02-04 18:59:39 +0100

    [DEV]

--- netlify/functions/puppet.js
+++ netlify/functions/puppet.js
@@ -5,7 +5,7 @@
  * Created Date: Sunday, February 4th 2024, 18:47:15
  * Author: Kirill Dorozhynskyi - kirilldy@justdev.org
  * -----
- * Last Modified: Sunday, February 4th 2024 18:53:54
+ * Last Modified: Sunday, February 4th 2024 18:59:33
  * Modified By: Kirill Dorozhynskyi
  * -----
  * Copyright (c) 2024 justDev
@@ -37,6 +37,28 @@ app.get("/api/hello", async (req, res) => {
     waitUntil: "networkidle2",
   });
   await page.setViewport({ width: 1239, height: 1753 });
+
+  const pdf = await page.pdf({
+    path: "output.pdf",
+    format: "A4",
+    printBackground: true,
+    displayHeaderFooter: false,
+
+    margin: {
+      top: "0",
+      right: "0",
+      bottom: "0",
+      left: "0",
+    },
+  });
+
+  await browser.close();
+  res.set({
+    "Content-Type": "application/pdf",
+    "Content-Length": pdf.length,
+  });
+  const filePath = path.join(__dirname, "output.pdf");
+  res.sendFile(filePath);
 
   res.send("Hello World!");
 });
