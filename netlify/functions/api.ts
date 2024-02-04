d37408fc320c7f983bfd8f1f6a0b2c634aeeb3d6
Kyrylo Dorozhynskyi <kyrylo.dorozhynskyi@justdev.org>
2024-02-04 18:11:54 +0100

    [DEV]

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -5,7 +5,7 @@
  * Created Date: Sunday, February 4th 2024, 17:24:04
  * Author: Kirill Dorozhynskyi - kirilldy@justdev.org
  * -----
- * Last Modified: Sunday, February 4th 2024 18:10:06
+ * Last Modified: Sunday, February 4th 2024 18:11:49
  * Modified By: Kirill Dorozhynskyi
  * -----
  * Copyright (c) 2024 justDev
@@ -29,21 +29,21 @@ router.get("/hello", async (req, res) => {
   });
   await page.setViewport({ width: 1239, height: 1753 });
 
-  const pdf = await page.pdf({
-    path: "output.pdf",
-    format: "A4",
-    printBackground: true,
-    displayHeaderFooter: false,
+  //   const pdf = await page.pdf({
+  //     path: "output.pdf",
+  //     format: "A4",
+  //     printBackground: true,
+  //     displayHeaderFooter: false,
 
-    margin: {
-      top: "0",
-      right: "0",
-      bottom: "0",
-      left: "0",
-    },
-  });
+  //     margin: {
+  //       top: "0",
+  //       right: "0",
+  //       bottom: "0",
+  //       left: "0",
+  //     },
+  //   });
 
-  await browser.close();
+  //   await browser.close();
   //   res.set({
   //     "Content-Type": "application/pdf",
   //     "Content-Length": pdf.length,
